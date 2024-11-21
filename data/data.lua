29ac47eb6d5b87a71142dd93b698cbc5fb3f0b58
Daniel Vazač <daniel.vazac@email.cz>
2024-11-21 23:28:38 +0100

    why

--- data/data.lua
+++ data/data.lua
@@ -1,81 +1,47 @@
 log("InfiniteResourcesDeposits: Begin")
 
-
-local iron_ore = data.raw.resource["iron-ore"]
 local copper_ore = data.raw.resource["copper-ore"]
 
-local infinite_resource = table.deepcopy(iron_ore)
-infinite_resource.name = "infinite-" .. iron_ore.name
-infinite_resource.infinite = true
-infinite_resource.minimum = iron_ore.minimum or 100
-infinite_resource.normal = iron_ore.normal or 100
-infinite_resource.infinite_depletion_amount = 0
-infinite_resource.minable.results = {
-    {
-        type = "item",
-        name = iron_ore.minable.result,
-        amount = 1
-    }
-}
--- data:extend({infinite_resource})
-
--- copper_ore.autoplace.richness_expression: "(var('control:copper-ore:size') > 0) * (1*var('control:copper-ore:richness')*(var('default-copper-ore-patches'))*max((1000+distance)/2600,1))"
--- copper_ore.autoplace.probability_expression : "(var('control:copper-ore:size') > 0) * (clamp(var('default-copper-ore-patches'), 0, 1))"
-
--- (var('control:copper-ore:size') > 0) * (1*var('control:copper-ore:richness')*(var('default-copper-ore-patches'))*max((1000+distance)/2600,1))
--- (var('control:copper-ore:size') > 0) * (1*var('control:copper-ore:richness')*(var('default-copper-ore-patches'))*max((1000+distance)/2600,1)) * (distance > 50)
-
--- resource_autoplace_all_patches{base_density = 8,base_spots_per_km2 = 2.5,candidate_spot_count = 22,frequency_multiplier = var('control:copper-ore:frequency'),has_starting_area_placement = 1,random_spot_size_minimum = 0.25,random_spot_size_maximum = 2,regular_blob_amplitude_multiplier = 0.125,regular_patch_set_count = default_regular_resource_patch_set_count,regular_patch_set_index = 1,regular_rq_factor = 0.11,seed1 = 100,size_multiplier = var('control:copper-ore:size'),starting_blob_amplitude_multiplier = 0.125,starting_patch_set_count = default_starting_resource_patch_set_count,starting_patch_set_index = 1,starting_rq_factor = 0.17142857142857}
-
-log("InfiniteResourcesDeposits: copper_ore.autoplace.richness_expression: " ..
-  serpent.block(iron_ore.autoplace.richness_expression))
-log("InfiniteResourcesDeposits: copper_ore.autoplace.probability_expression : " ..
-  serpent.block(iron_ore.autoplace.probability_expression))
-log("InfiniteResourcesDeposits: copper_ore.autoplace.local_expressions : " ..
-  serpent.block(iron_ore.autoplace.local_expressions))
-
+-- AutoplaceControl
 data:extend({
   {
-    expression =
-    "resource_autoplace_all_patches{ base_density = 8, base_spots_per_km2 = 2.5, candidate_spot_count = 22, frequency_multiplier = var('control:copper-ore:frequency'), has_starting_area_placement = 1, random_spot_size_minimum = 0.025, random_spot_size_maximum = 0.2, regular_blob_amplitude_multiplier = 0.0125, regular_patch_set_count = default_regular_resource_patch_set_count, regular_patch_set_index = 1, regular_rq_factor = 0.011, seed1 = 100, size_multiplier = var('control:copper-ore:size') * 0.1, starting_blob_amplitude_multiplier = 0.0125, starting_patch_set_count = default_starting_resource_patch_set_count, starting_patch_set_index = 1, starting_rq_factor = 0.017142857142857}",
-    name = "hole-copper-ore-patches",
-    type = "noise-expression",
+      type = "autoplace-control",
+      name = "infinite-copper-ore",
+      richness = true,
+      order = "a",
+      category = "resource"
   }
 })
 
--- iron_ore_infinite.autoplace.richness_expression =
--- "(var('control:copper-ore:size') > 0) * (1*var('control:copper-ore:richness')*(var('hole-copper-ore-patches'))*max((1000+distance)/2600,1))"
--- iron_ore_infinite.autoplace.probability_expression =
--- "(var('control:copper-ore:size') > 0) * (clamp(var('hole-copper-ore-patches'), 0, 1))"
--- iron_ore_infinite.autoplace.order = "a"
 
--- copper_ore.autoplace.richness_expression = 0
--- -- "(var('control:copper-ore:size') > 0) * (1*var('control:copper-ore:richness')*(var('copper-ore-patches'))*max((1000+distance)/2600,1))"
--- copper_ore.autoplace.probability_expression = 0
--- -- "(var('control:copper-ore:size') > 0) * (clamp(var('copper-ore-patches'), 0, 1))"
--- copper_ore.autoplace.order = "b"
-
--- iron_ore.autoplace.richness_expression = 0
--- -- "(var('control:copper-ore:size') > 0) * (1*var('control:copper-ore:richness')*(var('copper-ore-patches'))*max((1000+distance)/2600,1))"
--- iron_ore.autoplace.probability_expression = 0
--- -- "(var('control:copper-ore:size') > 0) * (clamp(var('copper-ore-patches'), 0, 1))"
--- iron_ore.autoplace.order = "b"
-
--- iron_ore.name = iron_ore.name
--- iron_ore.infinite = true
--- iron_ore.minimum = iron_ore.minimum or 100
--- iron_ore.normal = iron_ore.normal or 100
--- iron_ore.infinite_depletion_amount = 0
--- iron_ore.minable.results = {
+local infinite_resource = table.deepcopy(copper_ore)
+infinite_resource.name = "infinite-" .. copper_ore.name
+-- infinite_resource.autoplace.control = "infinite-copper-ore"
+infinite_resource.map_color = {r = 0.0, g = 1, b = 0.0}
+-- infinite_resource.localised_name = {"entity-name.infinite-resource", {"entity-name." .. copper_ore.name}}
+-- infinite_resource.infinite = true
+-- infinite_resource.minimum = copper_ore.minimum or 100
+-- infinite_resource.normal = copper_ore.normal or 100
+-- infinite_resource.infinite_depletion_amount = 0
+-- infinite_resource.minable.results = {
 --     {
 --         type = "item",
---         name = iron_ore.minable.result,
+--         name = copper_ore.minable.result,
 --         amount = 1
 --     }
 -- }
 
-iron_ore.autoplace.order = iron_ore.autoplace.order .. "-b" 
+infinite_resource.autoplace.richness_expression = "1"
+infinite_resource.autoplace.probability_expression = "1"
 
+-- copper_ore.autoplace.richness_expression = "1"
+-- copper_ore.autoplace.probability_expression = "1"
 
+infinite_resource.autoplace.order = "a"
+copper_ore.autoplace.order = "c" 
 
+data:extend({infinite_resource})
 log("InfiniteResourcesDeposits: End")
+
+log("Copper ore: " .. serpent.block(copper_ore))
+log("Infinite copper ore: " .. serpent.block(infinite_resource))
